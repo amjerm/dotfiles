b43b20d6b5ada76bb0d86cc66db75844b9e74619
Adam Jermstad <dev@amjerm.me>
2023-09-28 12:25:01 -0500

    hammerspoon: hide background apps

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,7 +3,7 @@ hs.hotkey.bind({ 'cmd', 'alt', 'ctrl' }, 'W', function()
 end)
 
 local appBindings = {
-        { 'B', 'firefox developer edition' }, -- browser
+        { 'W', 'firefox developer edition' }, -- web browser
         { 'C', 'fantastical' },               -- calendar
         { 'D', 'datagrip' },                  -- database
         { 'M', 'slack' },                     -- messaging
@@ -17,9 +17,33 @@ for _, v in pairs(appBindings) do
         end)
 end
 
+-- hide frontmost app
 hs.hotkey.bind({ 'cmd', 'ctrl' }, 'X', function()
         hs.application.frontmostApplication():hide()
 end)
+
+-- hide all apps
+hs.hotkey.bind({ 'cmd', 'ctrl' }, 'A', function()
+        hideAll(true)
+end)
+
+-- hide all except frontmost app
+hs.hotkey.bind({ 'cmd', 'ctrl' }, 'B', function()
+        hideAll(true)
+end)
+
+hideAll = function(excludeFrontmost)
+        local runningApps = hs.application.runningApplications()
+        for _, v in pairs(runningApps) do
+                if excludeFrontmost then
+                        if v:isFrontmost() == false then
+                                v:hide()
+                        end
+                else
+                        v:hide()
+                end
+        end
+end
 
 toggleApp = function(name)
         local app = hs.application.get(name)
