e9e4008a0d6a09ff7efc461d843dc3eff2b75a50
Adam Jermstad <dev@amjerm.me>
2023-09-28 09:24:19 -0500

    update hammerspoon bindings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,5 @@
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
-        hs.alert.show("Hello World!")
+hs.hotkey.bind({ 'cmd', 'alt', 'ctrl' }, 'W', function()
+        hs.alert.show('Hello World!')
 end)
 
 local appBindings = {
@@ -8,14 +8,18 @@ local appBindings = {
         { 'N', 'obsidian' },
         { 'P', 'firefox' },
         { 'S', 'slack' },
-        { 'T', 'telegram' },
+        { 'T', 'wezterm' },
 }
 
 for _, v in pairs(appBindings) do
-        hs.hotkey.bind({ "cmd", "ctrl" }, v[1], function()
+        hs.hotkey.bind({ 'cmd', 'ctrl' }, v[1], function()
                 toggleApp(v[2])
         end)
 end
+
+hs.hotkey.bind({ 'cmd', 'ctrl' }, 'X', function()
+        hs.application.frontmostApplication():hide()
+end)
 
 toggleApp = function(name)
         local app = hs.application.get(name)
