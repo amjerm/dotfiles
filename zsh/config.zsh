8afde283942a3f563c3e0e205c2c2f62257df872
Adam Jermstad <30279337+amjerm@users.noreply.github.com>
2023-10-06 17:08:35 -0500

    add several services

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -67,36 +67,15 @@ export EDITOR='nvim'
 export MANPAGER='nvim +Man!'
 export PAGER='nvim -R'
 export TIME_STYLE='long-iso'
+
+trysource "$XDG_CONFIG_HOME/zsh/zvm_config.zsh"
 trysource "$XDG_CONFIG_HOME/zsh/nnn_config.zsh"
 
 ############
 # ALIASES  #
 ############
 
-alias edit="$EDITOR"
-alias eg="edit $XDG_CONFIG_HOME/git/config"
-alias email="$EMAIL"
-alias en="edit $XDG_CONFIG_HOME/nvim"
-alias et="edit $XDG_CONFIG_HOME/tmux/tmux.conf"
-alias ew="edit $XDG_CONFIG_HOME/wezterm/wezterm.lua"
-alias ez="edit $ZDOTDIR/.zshrc"
-alias eza="eza --icons -a"
-alias fsc="npm run cl:scopes -- -q $1"
-alias lc="ls $XDG_CONFIG_HOME"
-alias lt="ls --tree --level=2"
-alias mem="cat $HOME/Documents/reference/memory-reference.txt"
-alias tk="tmux kill-ses"
-alias tl="tmux ls"
-alias tn="tmux new -s (pwd | sed 's/.*\///g')"
-alias weather="curl 'wttr.in/austin?mnQF1'"
-alias weathero="curl 'wttr.in/austin?m&format=%t+%p+%m+%M\n'"
-alias wtc="wezterm_set_color $1"
-alias wtcl="cat $XDG_CONFIG_HOME/wezterm/color_schemes.txt"
-alias wtf="set_wezterm_font $1"
-alias wtfs="grep font_size $XDG_CONFIG_HOME/wezterm/wezterm.lua"
-
-# OP Vault
-alias sgpt="op run -- sgpt"
+trysource "$XDG_CONFIG_HOME/zsh/aliases.zsh"
 
 ############
 # PLUGINS  #
--- zsh/nnn_config.zsh
+++ zsh/nnn_config.zsh
@@ -1,4 +1,17 @@
-BLK="0B" CHR="0B" DIR="04" EXE="06" REG="00" HARDLINK="06" SYMLINK="06" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
-export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+ BLK="0B" CHR="0B" DIR="27" EXE="06" REG="00" HARDLINK="06" SYMLINK="9a" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
+ export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
 
 export NNN_BMS="d:$HOME/code/tsi/docker;r:$HOME/code/tsi/docker/repos"
+
+export NNN_OPENER="$EDITOR"
+
+export NNN_TMPFILE="$XDG_STATE_HOME/nnn/.lastd"
+
+# change to last nnn directory
+ncd ()
+{
+    [ ! -f "$NNN_TMPFILE" ] || {
+        . "$NNN_TMPFILE"
+        rm -f "$NNN_TMPFILE" > /dev/null
+    }
+}
