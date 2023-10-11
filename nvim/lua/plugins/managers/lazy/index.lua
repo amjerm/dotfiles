3e16e54a5e6adf96c4976260ba2eafb2e96ca4f5
Adam Jermstad <30279337+amjerm@users.noreply.github.com>
2023-10-11 10:09:19 -0500

    nvim: conform for lsp format

--- nvim/lua/plugins/managers/lazy/index.lua
+++ nvim/lua/plugins/managers/lazy/index.lua
@@ -1,203 +1,204 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-                "git",
-                "clone",
-                "--filter=blob:none",
-                "https://github.com/folke/lazy.nvim.git",
-                "--branch=stable", -- latest stable release
-                lazypath,
-        })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-        --language server
-        'williamboman/mason-lspconfig.nvim',
-        'neovim/nvim-lspconfig',
-        'simrat39/rust-tools.nvim',
-        'jparise/vim-graphql',
-        'ray-x/lsp_signature.nvim',
-        'sbdchd/neoformat',
-        'lukas-reineke/lsp-format.nvim',
-        'jose-elias-alvarez/typescript.nvim',
-        {
-                "pmizio/typescript-tools.nvim",
-                dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-                opts = {},
-        },
-        {
-                'saecki/crates.nvim',
-                tag = 'v0.3.0',
-                dependencies = { 'nvim-lua/plenary.nvim' },
-        },
-        'b0o/schemastore.nvim',
-        {
-                'Wansmer/symbol-usage.nvim',
-                event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-        },
-        'williamboman/mason.nvim',
+	--language server
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"simrat39/rust-tools.nvim",
+	"jparise/vim-graphql",
+	"ray-x/lsp_signature.nvim",
+	-- 'sbdchd/neoformat',
+	-- 'lukas-reineke/lsp-format.nvim',
+	"stevearc/conform.nvim",
+	"jose-elias-alvarez/typescript.nvim",
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+	{
+		"saecki/crates.nvim",
+		tag = "v0.3.0",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	"b0o/schemastore.nvim",
+	{
+		"Wansmer/symbol-usage.nvim",
+		event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+	},
+	"williamboman/mason.nvim",
 
-        --treesitter
-        { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-        'nvim-treesitter/nvim-treesitter-textobjects',
+	--treesitter
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"nvim-treesitter/nvim-treesitter-textobjects",
 
-        --diagnostics
-        {
-                "folke/trouble.nvim",
-                dependencies = { "kyazdani42/nvim-web-devicons" },
-        },
+	--diagnostics
+	{
+		"folke/trouble.nvim",
+		dependencies = { "kyazdani42/nvim-web-devicons" },
+	},
 
-        --autocomplete
-        'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/cmp-git',
-        'hrsh7th/cmp-path',
-        'quangnguyen30192/cmp-nvim-ultisnips',
-        'onsails/lspkind.nvim',                --pictograms in cmp
-        'hrsh7th/cmp-nvim-lsp-signature-help', --signature help
-        'folke/neodev.nvim',                   --lua signature help
+	--autocomplete
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/cmp-git",
+	"hrsh7th/cmp-path",
+	"quangnguyen30192/cmp-nvim-ultisnips",
+	"onsails/lspkind.nvim", --pictograms in cmp
+	"hrsh7th/cmp-nvim-lsp-signature-help", --signature help
+	"folke/neodev.nvim", --lua signature help
 
-        --snippets
-        'SirVer/ultisnips',
-        'honza/vim-snippets',
+	--snippets
+	"SirVer/ultisnips",
+	"honza/vim-snippets",
 
-        --shortcuts
-        'tpope/vim-unimpaired',
+	--shortcuts
+	"tpope/vim-unimpaired",
 
-        --themes
-        'arcticicestudio/nord-vim',
-        'sainnhe/everforest',
-        'sainnhe/gruvbox-material',
-        'cideM/yui', -- for diffs
-        'mechatroner/rainbow_csv',
-        'rebelot/kanagawa.nvim',
-        { 'catppuccin/nvim',                 name = 'catppuccin', priority = 1000 },
-        { 'rose-pine/neovim',                name = 'rose-pine',  priority = 1000 },
-        { 'savq/melange-nvim',               name = 'melange',    priority = 1000 },
-        { 'embark-theme/vim',                name = 'embark',     priority = 1000 },
-        'ghifarit53/tokyonight-vim',
-        {
-                'AlexvZyl/nordic.nvim',
-                lazy = false,
-                priority = 1000,
-                config = function()
-                        require 'nordic'.load()
-                end
-        },
-        'whatyouhide/vim-gotham',
-        'tlhr/anderson.vim',
-        'ayu-theme/ayu-vim',
-        'AlessandroYorba/Alduin',
-        'rakr/vim-two-firewatch',
-        'AlessandroYorba/Sierra',
-        'haystackandroid/carbonized',
-        'jdsimcoe/abstract.vim',
-        'rose-pine/neovim',
-        'aktersnurra/no-clown-fiesta.nvim',
+	--themes
+	"arcticicestudio/nord-vim",
+	"sainnhe/everforest",
+	"sainnhe/gruvbox-material",
+	"cideM/yui", -- for diffs
+	"mechatroner/rainbow_csv",
+	"rebelot/kanagawa.nvim",
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+	{ "savq/melange-nvim", name = "melange", priority = 1000 },
+	{ "embark-theme/vim", name = "embark", priority = 1000 },
+	"ghifarit53/tokyonight-vim",
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
+		end,
+	},
+	"whatyouhide/vim-gotham",
+	"tlhr/anderson.vim",
+	"ayu-theme/ayu-vim",
+	"AlessandroYorba/Alduin",
+	"rakr/vim-two-firewatch",
+	"AlessandroYorba/Sierra",
+	"haystackandroid/carbonized",
+	"jdsimcoe/abstract.vim",
+	"rose-pine/neovim",
+	"aktersnurra/no-clown-fiesta.nvim",
 
-        --icons
-        'kyazdani42/nvim-web-devicons',
+	--icons
+	"kyazdani42/nvim-web-devicons",
 
-        --statusline
-        {
-                'nvim-lualine/lualine.nvim',
-                dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true }
-        },
-        'arkav/lualine-lsp-progress',
+	--statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "kyazdani42/nvim-web-devicons", lazy = true },
+	},
+	"arkav/lualine-lsp-progress",
 
-        --splash screen
-        {
-                'glepnir/dashboard-nvim',
-                dependencies = { 'kyazdani42/nvim-web-devicons' }
-        },
+	--splash screen
+	{
+		"glepnir/dashboard-nvim",
+		dependencies = { "kyazdani42/nvim-web-devicons" },
+	},
 
-        -- directory explorer
-        'stevearc/oil.nvim',
+	-- directory explorer
+	"stevearc/oil.nvim",
 
-        --filesystem commands
-        'tpope/vim-eunuch',
+	--filesystem commands
+	"tpope/vim-eunuch",
 
-        --symbol outline
-        'stevearc/aerial.nvim',
+	--symbol outline
+	"stevearc/aerial.nvim",
 
-        --marks
-        'ThePrimeagen/harpoon',
+	--marks
+	"ThePrimeagen/harpoon",
 
-        --comment toggle
-        'tpope/vim-commentary',
+	--comment toggle
+	"tpope/vim-commentary",
 
-        --search and replace
-        'tpope/vim-abolish',
+	--search and replace
+	"tpope/vim-abolish",
 
-        --git
-        'tpope/vim-fugitive',     --comprehensive git tool
-        'airblade/vim-gitgutter', --git status in gutter
+	--git
+	"tpope/vim-fugitive", --comprehensive git tool
+	"airblade/vim-gitgutter", --git status in gutter
 
-        --fuzzy finder
-        {
-                'junegunn/fzf',
-                dependencies = {
-                        'junegunn/fzf.vim',
-                },
-                build = '{ -> fzf#install() }',
-        },
+	--fuzzy finder
+	{
+		"junegunn/fzf",
+		dependencies = {
+			"junegunn/fzf.vim",
+		},
+		build = "{ -> fzf#install() }",
+	},
 
-        --telescope
-        {
-                'nvim-telescope/telescope.nvim',
-                version = '0.1.0',
-                dependencies = { { 'nvim-lua/plenary.nvim' } }
-        },
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+	--telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "0.1.0",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 
-        --repeat plugin commands with '.' in addition to native ones
-        'tpope/vim-repeat',
+	--repeat plugin commands with '.' in addition to native ones
+	"tpope/vim-repeat",
 
-        --refactor wrapping characters/tags
-        'tpope/vim-surround',
+	--refactor wrapping characters/tags
+	"tpope/vim-surround",
 
-        --auto-close brackets
-        'jiangmiao/auto-pairs',
+	--auto-close brackets
+	"jiangmiao/auto-pairs",
 
-        --split and join lines
-        'AndrewRadev/splitjoin.vim',
+	--split and join lines
+	"AndrewRadev/splitjoin.vim",
 
-        --detect buffer options
-        'tpope/vim-sleuth',
+	--detect buffer options
+	"tpope/vim-sleuth",
 
-        --debugger
-        'mfussenegger/nvim-dap',
-        'Pocco81/DAPInstall.nvim',
-        'rcarriga/nvim-dap-ui',
-        'theHamsta/nvim-dap-virtual-text',
-        'nvim-telescope/telescope-dap.nvim',
+	--debugger
+	"mfussenegger/nvim-dap",
+	"Pocco81/DAPInstall.nvim",
+	"rcarriga/nvim-dap-ui",
+	"theHamsta/nvim-dap-virtual-text",
+	"nvim-telescope/telescope-dap.nvim",
 
-        --testing
-        'vim-test/vim-test',
-        {
-                'nvim-neotest/neotest',
-                dependencies = {
-                        'nvim-lua/plenary.nvim',
-                        'nvim-neotest/neotest-plenary',
-                        'nvim-treesitter/nvim-treesitter',
-                        'antoinemadec/FixCursorHold.nvim',
-                        'nvim-neotest/neotest-vim-test',
-                        'haydenmeade/neotest-jest',
-                        'rouge8/neotest-rust',
-                }
-        },
+	--testing
+	"vim-test/vim-test",
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-neotest/neotest-plenary",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-neotest/neotest-vim-test",
+			"haydenmeade/neotest-jest",
+			"rouge8/neotest-rust",
+		},
+	},
 
-        --databases
-        'tpope/vim-dadbod',
+	--databases
+	"tpope/vim-dadbod",
 
-        --email
-        'https://git.sr.ht/~soywod/himalaya-vim',
+	--email
+	"https://git.sr.ht/~soywod/himalaya-vim",
 
-        --markdown preview
-        { 'toppair/peek.nvim',                        run = 'deno task --quiet build:fast' },
+	--markdown preview
+	{ "toppair/peek.nvim", run = "deno task --quiet build:fast" },
 }
 
-require('lazy').setup(plugins)
+require("lazy").setup(plugins)
--- nvim/lua/plugins/settings/lsp/formatting.lua
+++ nvim/lua/plugins/settings/lsp/formatting.lua
@@ -1,8 +1,30 @@
-require('lsp-format').setup()
+require("conform").setup({
+    formatters_by_ft = {
+        -- Conform will run multiple formatters sequentially
+        -- Use a sub-list to run only the first available formatter
+        lua = { "stylua" },
+        toml = {"taplo"},
+        javascript = { "rustywind", { "prettierd", "prettier" } },
+        javascriptreact = { "rustywind", { "prettierd", "prettier" } },
+        typescript = { "rustywind", { "prettierd", "prettier" } },
+        typescriptreact = { "rustywind", { "prettierd", "prettier" } },
+        rust = { "rustfmt" },
+        yaml = {"yamlfmt"},
+    },
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
+})
 
 -- neoformat
-vim.g.neoformat_try_node_exe = 1
-vim.cmd [[ autocmd BufWritePre *.{js,jsx,ts,tsx} Neoformat ]]
+-- require('lsp-format').setup()
+-- vim.g.neoformat_try_node_exe = 1
+-- vim.cmd [[ autocmd BufWritePre *.{js,jsx,ts,tsx} Neoformat ]]
 
 -- default format on save (includes rustfmt)
-vim.cmd [[ autocmd BufWritePre *.{rs,html,php} lua vim.lsp.buf.format() ]]
+-- vim.cmd [[ autocmd BufWritePre *.{rs,html,php} lua vim.lsp.buf.format() ]]
+
